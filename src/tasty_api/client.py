35a5c3b5c854ff4963bbfa1e030974ef7ab5a6c4
keysdapp <keysdapp@gmail.com>
2024-05-26 21:11:15 -0700

    Add Client
    Client doesn't have all the endpoints yet but it has all the recipes/ ones.

---
+++ src/tasty_api/client.py
@@ -0,0 +1,127 @@
+from enum import StrEnum
+
+import requests
+
+from tasty_api.constants import TASTY_HOST
+from tasty_api.data import CompletionData, RecipeListData
+from tasty_api.recipe import Completion, Recipe
+from tasty_api.tag import Tag, tag_list_to_str
+
+
+class SortingMethod(StrEnum):
+    POPULAR = "popular"
+    DESCENDING = "approved_at:desc"
+    ASCENDING = "approved_at:asc"
+
+
+class Client:
+    """A client to send requests to the Tasty API."""
+
+    __slots__ = "_session"
+
+    def __init__(self, rapid_api_key: str) -> None:
+        """
+        A client to send requests to the Tasty API.
+
+        :param str rapid_api_key: Your API key. Get one from https://rapidapi.com/apidojo/api/tasty.
+        """
+
+        self._session = requests.Session()
+        self._session.headers.update(
+            {
+                "X-RapidAPI-Key": rapid_api_key,
+                "X-RapidAPI-Host": TASTY_HOST,
+            }
+        )
+
+    def get_recipes_auto_complete(self, prefix: str) -> list[Completion]:
+        """
+        Get auto complete suggestions by name or ingredients.
+
+        :param str prefix: The text that will be completed.
+        :return list[Completion]: A list of possible auto completions.
+        """
+
+        url = "https://tasty.p.rapidapi.com/recipes/auto-complete"
+
+        querystring = {"prefix": prefix}
+
+        response = self._session.get(url, params=querystring)
+
+        data = CompletionData.from_dict(response.json())
+
+        return data.results
+
+    def get_recipes_list(
+        self,
+        offset: int,
+        size: int,
+        tags: list[Tag] | None = None,
+        query: str | None = None,
+        sort: SortingMethod = SortingMethod.POPULAR,
+    ) -> list[Recipe]:
+        """
+        Get a list of recipes.
+
+        :param int offset: The amount of recipes to skip.
+        :param int size: The amount of recipes to get.
+        :param list[Tag] | None tags: A list of tags you want to search for, defaults to None
+        :param str | None query: Name of food or ingredients to search by, defaults to None
+        :param SortingMethod sort: The method of sorting the results, defaults to SortingMethod.POPULAR
+        :return list[Recipe]: The list of recipes.
+        """
+
+        url = "https://tasty.p.rapidapi.com/recipes/list"
+
+        querystring = {"from": str(offset), "size": str(size)}
+
+        if tags is not None:
+            querystring.update({"tags": tag_list_to_str(tags)})
+
+        if query is not None:
+            querystring.update({"q": query})
+
+        if sort != SortingMethod.POPULAR:
+            querystring.update({"sort": sort})
+
+        response = self._session.get(url, params=querystring)
+
+        data = RecipeListData.from_dict(response.json())
+
+        return data.results
+
+    def get_recipes_list_similarities(self, recipe_id: int) -> list[Recipe]:
+        """
+        Get a list of recipes similar to a given recipe.
+
+        :param int recipe_id: The recipe to get similar recipes to.
+        :return list[Recipe]: The list of similar recipes.
+        """
+
+        url = "https://tasty.p.rapidapi.com/recipes/list-similarities"
+
+        querystring = {"recipe_id": recipe_id}
+
+        response = self._session.get(url, params=querystring)
+
+        data = RecipeListData.from_dict(response.json())
+
+        return data.results
+
+    def get_recipes_more_info(self, recipe_id: int) -> Recipe:
+        """
+        Get a specific recipe.
+
+        :param int recipe_id: The recipe to get information on.
+        :return Recipe: The recipe.
+        """
+
+        url = "https://tasty.p.rapidapi.com/recipes/get-more-info"
+
+        querystring = {"recipe_id": recipe_id}
+
+        response = self._session.get(url, params=querystring)
+
+        recipe = Recipe.from_dict(response.json())
+
+        return recipe
